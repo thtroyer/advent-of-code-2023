d5ad07156378b1ce371b000338f4fadde3502f28
Tom Troyer <tom.troyer@gmail.com>
2023-12-01 14:50:24 -0500

    Part 2 progress

--- src/main/java/thtroyer/day1/Day1.java
+++ src/main/java/thtroyer/day1/Day1.java
@@ -11,6 +11,7 @@ public class Day1 {
     }
 
     protected int getCalibrationValue(String line) {
+        line = convertEmbededNumbers(line);
         var numbers = line.chars()
                 .mapToObj(i -> (char)i)
                 .filter(Character::isDigit)
@@ -18,6 +19,19 @@ public class Day1 {
                 .toList();
 
         return Integer.parseInt(numbers.getFirst() + numbers.getLast());
+    }
+
+    protected String convertEmbededNumbers(String line) {
+        line = line.replace("one", "1");
+        line = line.replace("two", "2");
+        line = line.replace("three", "3");
+        line = line.replace("four", "4");
+        line = line.replace("five", "5");
+        line = line.replace("six", "6");
+        line = line.replace("seven", "7");
+        line = line.replace("eight", "8");
+        line = line.replace("nine", "9");
+        return line;
     }
 
     public static void main(String[] args) {
--- src/test/java/thtroyer/day1/Day1Test.java
+++ src/test/java/thtroyer/day1/Day1Test.java
@@ -35,7 +35,15 @@ class Day1Test {
                 Arguments.of("1abc2", 12),
                 Arguments.of("pqr3stu8vwx", 38),
                 Arguments.of("a1b2c3d4e5f", 15),
-                Arguments.of("treb7uchet", 77)
+                Arguments.of("treb7uchet", 77),
+                // part 2
+                Arguments.of("two1nine", 29),
+                Arguments.of("eightwothree", 83),
+                Arguments.of("abcone2threexyz", 13),
+                Arguments.of("xtwone3four", 24),
+                Arguments.of("4nineeightseven2", 42),
+                Arguments.of("zoneight234", 14),
+                Arguments.of("7pqrstsixteen", 76)
         );
     }
 
@@ -45,8 +53,15 @@ class Day1Test {
                         1abc2
                         pqr3stu8vwx
                         a1b2c3d4e5f
-                        treb7uchet
-                        """, 142)
+                        treb7uchet""", 142),
+                Arguments.of("""
+                        two1nine
+                        eightwothree
+                        abcone2threexyz
+                        xtwone3four
+                        4nineeightseven2
+                        zoneight234
+                        7pqrstsixteen""", 281)
         );
     }
 }
