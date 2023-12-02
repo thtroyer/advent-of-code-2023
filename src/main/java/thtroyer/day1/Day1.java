a7a2272496b0529462cca6157ac666a32f4e7261
Tom Troyer <tom.troyer@gmail.com>
2023-12-01 22:24:58 -0500

    Part2 almost working

--- src/main/java/thtroyer/day1/Day1.java
+++ src/main/java/thtroyer/day1/Day1.java
@@ -1,8 +1,21 @@
 package thtroyer.day1;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Day1 {
+    record Match(int index, String name, int value) {}
+
+    Map<String, Integer> numbers = Map.of(
+           "one", 1,
+           "two", 2,
+           "three", 3,
+           "four", 4,
+           "five", 5,
+           "six", 6,
+           "seven", 7,
+           "eight", 8,
+           "nine", 9 );
+
     int getCalibrationValue(String ... lines) {
         return Arrays.stream(lines)
                 .map(this::getCalibrationValue)
@@ -11,7 +24,9 @@ public class Day1 {
     }
 
     protected int getCalibrationValue(String line) {
-        line = convertEmbededNumbers(line);
+        line = replaceFirst(line);
+        line = replaceLast(line);
+
         var numbers = line.chars()
                 .mapToObj(i -> (char)i)
                 .filter(Character::isDigit)
@@ -21,18 +36,41 @@ public class Day1 {
         return Integer.parseInt(numbers.getFirst() + numbers.getLast());
     }
 
-    protected String convertEmbededNumbers(String line) {
-        line = line.replace("one", "1");
-        line = line.replace("two", "2");
-        line = line.replace("three", "3");
-        line = line.replace("four", "4");
-        line = line.replace("five", "5");
-        line = line.replace("six", "6");
-        line = line.replace("seven", "7");
-        line = line.replace("eight", "8");
-        line = line.replace("nine", "9");
-        return line;
+    private String replaceFirst(String line) {
+        var match = findFirstNumberString(line);
+        return match
+                .map(value -> line.replaceFirst(value.name(), String.valueOf(value.value)))
+                .orElse(line);
     }
+
+    private String replaceLast(String line) {
+        var match = findLastNumberString(line);
+        return match
+                .map(value -> reverse(
+                        reverse(line).replaceFirst(
+                                reverse(value.name()),
+                                String.valueOf(value.value))))
+                .orElse(line);
+    }
+
+    private String reverse(String line) {
+        return new StringBuilder(line).reverse().toString();
+    }
+
+    private Optional<Match> findFirstNumberString(String line) {
+        return numbers.entrySet().stream()
+                .map(m -> new Match(line.indexOf(m.getKey()), m.getKey(), m.getValue()))
+                .filter(m -> m.index() != -1)
+                .min(Comparator.comparingInt(Match::index));
+    }
+
+    private Optional<Match> findLastNumberString(String line) {
+        return numbers.entrySet().stream()
+                .map(m -> new Match(line.indexOf(m.getKey()), m.getKey(), m.getValue()))
+                .filter(m -> m.index() != -1)
+                .max(Comparator.comparingInt(Match::value));
+    }
+
 
     public static void main(String[] args) {
         System.out.println(new Day1().getCalibrationValue(args));
